3570b5453189cd50d6846ccce4396313fbd4b178
Mark Madsen <mark@madsenlab.org>
2016-05-07 14:06:11 -0700

    Switch to grid engine versions of all scripts

--- experiments/ni-pnn-1000/runall.sh
+++ experiments/ni-pnn-1000/runall.sh
@@ -7,14 +7,13 @@ set -o errexit
 sh bin/build-networkmodel.sh
 
 sh bin/build-simulations.sh
-sh bin/run-simulations.sh
-#sh bin/run-simulations-gridengine.sh
+sh bin/run-simulations-gridengine.sh
 
-sh bin/export-data.sh
-sh bin/simulation-postprocess.sh
+sh bin/export-data-gridengine.sh
+sh bin/simulation-postprocess-gridengine.sh
+
 
 sh bin/build-seriations.sh
-#sh bin/run-seriations-gridengine.sh
-sh bin/run-seriations.sh
+sh bin/run-seriations-gridengine.sh
 
 sh bin/annotate-seriation-output.sh
